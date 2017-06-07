6cef0ff1d11a80ccd8b4ec5ec2eb159bc251c2bc
Xusen Yin <yinxusen@gmail.com>
2017-06-07 14:11:55 -0700

    update shihbot

---
+++ test-shihbot.py
@@ -0,0 +1,61 @@
+"""
+Simple Facebook Echo bot: Respond with exactly what it receives
+Standalone version
+"""
+
+import sys, json, traceback, requests
+import textwrap
+import time
+import random
+
+# read conf
+npc_server = 'http://127.0.0.1:10000/SHIHbot/'
+
+def processIncoming(message):
+    data = {
+        "q": message,
+        "from": 'test'
+    }
+    try:
+        r = requests.post(npc_server, data=data)
+    except ConnectionError:
+        return 'I\'m away from keyboard, will come back in a moment, your message {}'.format(message_text)
+
+    if r.status_code != requests.codes.ok:
+        return 'I\'m away from keyboard, will come back in a moment'
+    if r.text == '':
+        return 'I don\'t understand your question: {}, try a different one?'.format(message_text)
+    return r.text
+
+
+def send_message(token, user_id, text):
+    """Send the message text to recipient with id recipient.
+    """
+    new_sentences = textwrap.fill(text, 300).split('\n')
+    for s in new_sentences:
+        time.sleep(random.randint(0, 2))
+        r = requests.post("https://graph.facebook.com/v2.6/me/messages",
+                          params={"access_token": token},
+                          data=json.dumps({
+                              "recipient": {"id": user_id},
+                              "message": {"text": s}
+                          }),
+                          headers={'Content-type': 'application/json'})
+        if r.status_code != requests.codes.ok:
+            print r.text
+
+
+def readBatchMessages(path):
+    with open(path, 'r') as qs:
+        questions = map(lambda q: q.strip(), qs.readlines())
+    return questions
+
+
+# Allows running with simple `python <filename> <port>`
+if __name__ == '__main__':
+    qs = readBatchMessages('/Users/panda/Downloads/nyQ100')
+    results = map(lambda q: processIncoming(q), qs)
+    for i, (q, r) in enumerate(zip(qs, results)):
+        print 'question {}: {}'.format(i, q)
+        print 'answer {}: {}'.format(i, r.encode('utf-8'))
+        print ''
